4ea22d5c398a826a006e32de413774b60b731970
yusuferen.uneyler <yusuferen.uneyler@dijitalkurye.com.tr>
2024-03-13 01:52:11 +0300

    jsondan veriler çekildi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
-
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDatatableModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+interface CombinedData {
+  il: string;
+  ilce: string;
+  ilce_id: string;
+}
+interface Il {
+  id: string;
+  name: string;
+}
 
+interface Ilce {
+  id: string;
+  il_id: string;
+  name: string;
+}
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
-export class MainpageComponent {
+export class MainpageComponent implements OnInit {
+  
+  ilList: Il[] = [];
+  ilceList: Ilce[] = [];
+  combinedList: CombinedData[] = [];
+  pageSize = 10;
+  offset = 0; 
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get<Il[]>('assets/iller.json').subscribe(ilData => {
+      this.http.get<Ilce[]>('assets/ilceler.json').subscribe(ilceData => {
+        this.ilList = ilData;
+        this.ilceList = ilceData;
+        this.combineData();
+        console.log(this.combinedList);
+        
+      });
+    });
+  }
+  combineData() {
+    this.combinedList = [];
+    for (const ilce of this.ilceList) {
+      const il = this.ilList.find(item => item.id === ilce.il_id);
+      if (il) {
+        this.combinedList.push({ il: il.name, ilce: ilce.name, ilce_id: ilce.id });
+      }
+    }
+  }
 
+  toggleEdit(row) {
+    row.editing = !row.editing;
+  }
+
+  save(row) {
+    row.editing = false;
+  }
+  onIlChange(row) {
+    // Seçilen ilin detaylarını yükle
+    // row.selectedIl ile seçilen ilin ID'sine erişebilirsiniz
+  }
 }
