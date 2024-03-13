4aeed23da487bae019b4c64086da8c08c83b1839
yusuferen.uneyler <yusuferen.uneyler@dijitalkurye.com.tr>
2024-03-13 17:11:24 +0300

    update eklendi

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -27,15 +27,14 @@ export class MainpageComponent implements OnInit {
   combinedList: CombinedData[] = [];
   pageSize = 10;
   offset = 0; 
+  changedIlce
   constructor(private http: HttpClient) {}
   ngOnInit() {
     this.http.get<Il[]>('assets/iller.json').subscribe(ilData => {
       this.http.get<Ilce[]>('assets/ilceler.json').subscribe(ilceData => {
-        this.ilList = ilData;
+        this.ilList = ilData.sort((a, b) => parseInt(a.id) - parseInt(b.id));
         this.ilceList = ilceData;
         this.combineData();
-        console.log(this.combinedList);
-        
       });
     });
   }
@@ -48,16 +47,33 @@ export class MainpageComponent implements OnInit {
       }
     }
   }
+  updateDistrict(event, row){
+    this.changedIlce = row.ilce_id
+    row.ilce = event.target.value
+    
+  }
+  updateDistrictId(event ,row){
+    this.changedIlce = row.ilce_id
+    row.ilce_id = event.target.value
 
+  }
   toggleEdit(row) {
     row.editing = !row.editing;
   }
 
   save(row) {
     row.editing = false;
+    console.log(row);
+    window.alert((row.ilce + ' ' + row.ilce_id))
+    
+  }
+
+  saveAll(){
+    console.log(this.combinedList);
+    window.alert(this.combinedList)
   }
   onIlChange(row) {
-    // Seçilen ilin detaylarını yükle
-    // row.selectedIl ile seçilen ilin ID'sine erişebilirsiniz
+    console.log(row);
+    
   }
 }
